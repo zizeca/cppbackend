13329118620e0db5415db50ba84ff0ecee2361b7
Kulametov Enver <zizu.meridian@gmail.com>
2022-11-03 16:59:16 +0300

    samples

---
+++ sprint1/samples/tcp-udp/src/tcp_server.cpp
@@ -0,0 +1,56 @@
+/**
+ * @file main.cpp
+ * @author Enver Kulametov (zizu.meridian@gmail.com)
+ * @brief
+ * @version 0.1
+ * @date 2022-11-03
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
+#include <boost/asio.hpp>
+#include <iostream>
+#include <string>
+#include <string_view>
+
+namespace net = boost::asio;
+using net::ip::tcp;
+
+using namespace std::literals;
+
+int main() {
+  static const int port = 3333;
+
+  net::io_context io_context;
+
+  tcp::acceptor acceptor(io_context, tcp::endpoint(tcp::v4(), port));
+  std::cout << "Waiting for connection..."sv << std::endl;
+
+  boost::system::error_code ec;
+  tcp::socket socket{io_context};
+  acceptor.accept(socket, ec);
+
+  if (ec) {
+    std::cout << "Can't accept connection"sv << std::endl;
+    return 1;
+  }
+
+  net::streambuf stream_buf;
+  net::read_until(socket, stream_buf, '\n', ec);
+  std::string client_data{std::istreambuf_iterator<char>(&stream_buf), std::istreambuf_iterator<char>()};
+
+  if (ec) {
+    std::cout << "Error reading data"sv << std::endl;
+    return 1;
+  }
+
+  std::cout << "Client said: "sv << client_data << std::endl;
+
+  socket.write_some(net::buffer("Hello, I'm server!\n"sv), ec);
+
+  if (ec) {
+    std::cout << "Error sending data"sv << std::endl;
+    return 1;
+  }
+}
