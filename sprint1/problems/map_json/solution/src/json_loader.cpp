b1b772a6f0dbbbd61da8b12067b39cc09c0fc48e
Kulametov Enver <zizu.meridian@gmail.com>
2022-11-14 11:27:10 +0300

    tag_invoce compiled

--- sprint1/problems/map_json/solution/src/json_loader.cpp
+++ sprint1/problems/map_json/solution/src/json_loader.cpp
@@ -8,7 +8,6 @@
 #include <string>
 #include <string_view>
 
-
 using namespace std::literals;
 
 /**  json map
@@ -137,16 +136,16 @@ void parseMap(const boost::json::value& val, model::Map& map) {
       continue;
     } else if (i->key() == "roads") {
       parseRoads(*p_arr, map);
-      std::cout << "Road\n";
+      //std::cout << "Road\n";
     } else if (i->key() == "buildings") {
       parseBuilding(*p_arr, map);
-      std::cout << "buildings\n";
+      //std::cout << "buildings\n";
     } else if (i->key() == "offices") {
       parseOffice(*p_arr, map);
-      std::cout << "offices\n";
+      //std::cout << "offices\n";
     } else {
-      std::cout << "unknon object " << i->key() << std::endl;
-      throw std::logic_error( "Found unknon json object"s + i->key_c_str());
+      //std::cout << "unknon object " << i->key() << std::endl;
+      throw std::logic_error("Found unknon json object"s + i->key_c_str());
     }
   }
 }
@@ -154,6 +153,8 @@ void parseMap(const boost::json::value& val, model::Map& map) {
 }  // namespace
 
 namespace json_loader {
+
+namespace json = boost::json;
 
 model::Game LoadGame(const std::filesystem::path& json_path) {
   // Загрузить содержимое файла json_path, например, в виде строки
@@ -200,3 +201,87 @@ model::Game LoadGame(const std::filesystem::path& json_path) {
 }
 
 }  // namespace json_loader
+
+
+void tag_invoke(boost::json::value_from_tag, boost::json::value& jv, model::Building const& building) {
+  jv = {{"x", std::to_string(building.GetBounds().position.x)},
+        {"y", std::to_string(building.GetBounds().position.y)},
+        {"w", std::to_string(building.GetBounds().size.width)},
+        {"x", std::to_string(building.GetBounds().size.height)}};
+}
+
+void tag_invoke(boost::json::value_from_tag, boost::json::value& jv, model::Office const& office) {
+  jv = {{"id", *office.GetId()},
+        {"x", std::to_string(office.GetPosition().x)},
+        {"y", std::to_string(office.GetPosition().y)},
+        {"offsetX", std::to_string(office.GetOffset().dx)},
+        {"offsetY", std::to_string(office.GetOffset().dy)}};
+}
+
+/**/
+void tag_invoke(boost::json::value_from_tag, boost::json::value& jv, model::Road const& road) {
+  boost::json::object obj;
+  obj["x0"] = std::to_string(road.GetStart().x);
+  obj["y0"] = std::to_string(road.GetStart().y);
+  if (road.IsHorizontal()) {
+    obj["x1"] = std::to_string(road.GetEnd().x);
+  } else {
+    obj["y1"] = std::to_string(road.GetEnd().y);
+  }
+
+  jv = boost::json::value_from(obj);
+}
+
+// костыли, 
+void tag_invoke(boost::json::value_from_tag, boost::json::value& jv, model::Map const& map) {
+  boost::json::object obj;
+  obj["id"] = *map.GetId();
+  obj["name"] = map.GetName();
+
+  // roads brief method not working :(
+  if (!map.GetRoads().empty()) {
+    boost::json::array arr;
+    for (auto& i : map.GetRoads()) {
+      // auto v = boost::json::value_from(r);
+      // arr.push_back(boost::json::value_from(i));
+      boost::json::object vobj;
+      vobj["x0"] = std::to_string(i.GetStart().x);
+      vobj["y0"] = std::to_string(i.GetStart().y);
+      if (i.IsHorizontal()) {
+        vobj["x1"] = std::to_string(i.GetEnd().x);
+      } else {
+        vobj["y1"] = std::to_string(i.GetEnd().y);
+      }
+      arr.push_back(boost::json::value_from(vobj));
+    }
+    obj["roads"] = boost::json::value_from(arr);
+  }
+
+  if (!map.GetBuildings().empty()) {
+    boost::json::array arr;
+    for (auto& i : map.GetBuildings()) {
+      boost::json::value v;
+      v = {{"x", std::to_string(i.GetBounds().position.x)},
+           {"y", std::to_string(i.GetBounds().position.y)},
+           {"w", std::to_string(i.GetBounds().size.width)},
+           {"h", std::to_string(i.GetBounds().size.height)}};
+      arr.push_back(v);
+    }
+    obj["buildings"] = boost::json::value_from(arr);
+  }
+
+  if (!map.GetOffices().empty()) {
+    boost::json::array arr;
+    for (auto& office : map.GetOffices()) {
+      boost::json::value v;
+      v = {{"id", *office.GetId()},
+            {"x", std::to_string(office.GetPosition().x)},
+            {"y", std::to_string(office.GetPosition().y)},
+            {"offsetX", std::to_string(office.GetOffset().dx)},
+            {"offsetY", std::to_string(office.GetOffset().dy)}};
+      arr.push_back(v);
+    }
+    obj["offices"] = boost::json::value_from(arr);
+  }
+  jv = obj;
+}
--- sprint1/problems/map_json/solution/src/json_loader.h
+++ sprint1/problems/map_json/solution/src/json_loader.h
@@ -1,11 +1,24 @@
 #pragma once
 
 #include <filesystem>
+#include <string_view>
 
 #include "model.h"
+
+#include <boost/json.hpp>
 
 namespace json_loader {
 
 model::Game LoadGame(const std::filesystem::path& json_path);
 
 }  // namespace json_loader
+
+void tag_invoke(boost::json::value_from_tag, boost::json::value& jv, model::Building const& building);
+void tag_invoke(boost::json::value_from_tag, boost::json::value& jv, model::Office const& office);
+void tag_invoke(boost::json::value_from_tag, boost::json::value& jv, model::Road const& road);
+void tag_invoke(boost::json::value_from_tag, boost::json::value& jv, model::Map const& map);
+
+//model::Building tag_invoke(boost::json::value_to_tag<model::Building>, boost::json::value& jv);
+//model::Office tag_invoke(boost::json::value_to_tag<model::Office>, boost::json::value& jv);
+//model::Road tag_invoke(boost::json::value_to_tag<model::Road>, boost::json::value& jv);
+//model::Map tag_invoke(boost::json::value_to_tag<model::Map>, boost::json::value& jv);
