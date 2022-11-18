be30a6724c7b965a361f238787de53df13c606a1
Kulametov Enver <zizu.meridian@gmail.com>
2022-11-19 01:03:34 +0300

    clear project

--- sprint1/problems/map_json/solution/src/request_handler.h
+++ sprint1/problems/map_json/solution/src/request_handler.h
@@ -10,27 +10,20 @@
 namespace http_handler {
 namespace beast = boost::beast;
 namespace http = beast::http;
-namespace js = boost::json;
+namespace json = boost::json;
 using namespace std::literals;
 // using namespace tag_invokers;
 
 using StringResponse = http::response<http::string_body>;
 
-void tag_invoke(boost::json::value_from_tag, boost::json::value& jv, model::Building const& building);
 
 // inner ns
 namespace {
 
 struct ErrorStr {
   ErrorStr() = delete;
-  constexpr static std::string_view MAP_NOT_FOUND = R"({
-  "code": "mapNotFound",
-  "message": "Map not found"
-})"sv;
-  constexpr static std::string_view BAD_REQ = R"({
-  "code": "badRequest",
-  "message": "Bad request"
-})"sv;
+  constexpr static std::string_view MAP_NOT_FOUND = R"({"code": "mapNotFound", "message": "Map not found"})"sv;
+  constexpr static std::string_view BAD_REQ = R"({"code": "badRequest", "message": "Bad request"})"sv;
 };
 
 struct ContentType {
@@ -39,7 +32,7 @@ struct ContentType {
   constexpr static std::string_view APP_JSON = "application/json"sv;
 };
 
-StringResponse MakeStringResponse(http::status status, std::string_view body, unsigned http_version, bool keep_alive,
+inline StringResponse MakeStringResponse(http::status status, std::string_view body, unsigned http_version, bool keep_alive,
                                   std::string_view content_type = ContentType::TEXT_HTML) {
   StringResponse response(status, http_version);
   response.set(http::field::content_type, content_type);
@@ -51,8 +44,6 @@ StringResponse MakeStringResponse(http::status status, std::string_view body, un
 
 }  // namespace
 
-// костыли,
-js::value invoke(model::Map const& map);
 
 class RequestHandler {
  public:
@@ -80,15 +71,15 @@ class RequestHandler {
       text_response(http::status::bad_request, ErrorStr::BAD_REQ);
       return;
     }
-
+    
     if (target == "/api/v1/maps") {
-      boost::json::array arr;
+      json::array arr;
       for (auto i : game_.GetMaps()) {
-        boost::json::value v = {{"id", *i.GetId()}, {"name", i.GetName()}};
+        json::value v = {{"id", *i.GetId()}, {"name", i.GetName()}};
         arr.push_back(v);
       }
-      boost::json::value v = arr;
-      text_response(http::status::ok, std::string(boost::json::serialize(v)));
+      json::value v = arr;
+      text_response(http::status::ok, std::string(json::serialize(v)));
       return;
     }
 
@@ -96,12 +87,11 @@ class RequestHandler {
       std::string s = target.substr(("/api/v1/maps/"s).size());
       auto m = game_.FindMap(model::Map::Id(s));
       if (m) {
-
-        js::value v = js::value_from(*m);
-        text_response(http::status::ok, js::serialize(v));
+        json::value v = json::value_from(*m);
+        text_response(http::status::ok, json::serialize(v));
         return;
       } else {
-        text_response((http::status)404, ErrorStr::MAP_NOT_FOUND);
+        text_response(http::status::not_found, ErrorStr::MAP_NOT_FOUND);
         return;
       }
     }
