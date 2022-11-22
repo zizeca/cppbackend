ee7aa7fcc840d1f5164c24a59ef2ddbea9e82964
Kulametov Enver <zizu.meridian@gmail.com>
2022-11-22 14:31:32 +0300

    Project reorganization.

--- sprint2/problems/static_content/solution/src/http_server.h
+++ sprint2/problems/static_content/solution/src/http_server.h
@@ -1,33 +1,17 @@
 /**
- * @file ServeHttp.hpp
+ * @file http_server.h
  * @author Enver Kulametov (zizu.meridian@gmail.com)
- * @brief 
+ * @brief File for providing access to the http server
  * @version 0.1
- * @date 2022-11-10
- * 
+ * @date 2022-11-22
+ *
  * @copyright Copyright (c) 2022
- * 
+ *
  */
+#ifndef __HTTP_SERVER_H__
+#define __HTTP_SERVER_H__
 
-#ifndef __SERVERHTTP_HPP__
-#define __SERVERHTTP_HPP__
+// include main part of server, for information handling
+#include "http_server/ServeHttp.hpp"
 
-#include <boost/asio/ip/tcp.hpp>
-#include <memory>
-
-#include "Listener.hpp"
-
-namespace http_server {
-
-template <typename RequestHandler>
-inline void ServeHttp(boost::asio::io_context& ioc, const boost::asio::ip::tcp::endpoint& endpoint, RequestHandler&& handler) {
-  // При помощи decay_t исключим ссылки из типа RequestHandler,
-  // чтобы Listener хранил RequestHandler по значению
-  using MyListener = Listener<std::decay_t<RequestHandler>>;
-
-  std::make_shared<MyListener>(ioc, endpoint, std::forward<RequestHandler>(handler))->Run();
-}
-
-}  // namespace http_server
-
-#endif // __SERVERHTTP_HPP__
+#endif // __HTTP_SERVER_H__
