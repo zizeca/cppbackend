e8cb0625a1cffa0a7545151dd139beed77d72972
Kulametov Enver <zizu.meridian@gmail.com>
2022-12-03 15:37:04 +0300

    vector operators

--- sprint2/problems/move_players/solution/src/model/model_types.h
+++ sprint2/problems/move_players/solution/src/model/model_types.h
@@ -29,6 +29,33 @@ struct Vector2 {
   Vector2(const T& X, const T& Y) : x(X), y(Y) {}
   T x;
   T y;
+
+  Vector2& operator+=(const Vector2<T>& other) {
+    x += other.x;
+    y += other.y;
+    return *this;
+  }
+
+  friend Vector2 operator+(Vector2<T> lhs, const Vector2<T>& rhs) {
+    lhs += rhs;
+    return lhs;
+  }
+
+  Vector2& operator*=(const T& scalar) {
+    x *= scalar;
+    y *= scalar;
+    return *this;
+  }
+
+  friend Vector2 operator*(Vector2<T> lhs, const T& scalar) {
+    lhs *= scalar;
+    return lhs;
+  }
+
+  friend Vector2 operator*(const T& scalar, Vector2<T> lhs) {
+    lhs *= scalar;
+    return lhs;
+  }
 };
 
 using Vector2i = Vector2<int>;
