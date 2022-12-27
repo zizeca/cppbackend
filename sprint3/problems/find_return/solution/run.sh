a3d2a39965318c5c3cc34cc53acb2762f8ffbcd1
Kulametov Enver <zizu.meridian@gmail.com>
2022-12-27 13:24:19 +0300

    Revert "prepare project find_return"
    This reverts commit dd31169fa3baa5d207f95cdeb90b54f5cd0da6e0.

--- sprint3/problems/find_return/solution/run.sh
+++
@@ -1,1 +0,0 @@
-./build/bin/game_server --config-file ./data/config.json --www-root static/ --tick-period 50 --randomize-spawn-points 1
--- sprint3/problems/find_return/solution/src/model.h
+++
@@ -1,8 +0,0 @@
-#ifndef __MODEL_H__
-#define __MODEL_H__
-
-#include "model/game.h"
-#include "model/player_list.h"
-// #include "model/tag_invokers.h"
-
-#endif // __MODEL_H__
