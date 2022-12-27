417bb6c9e168f19e58f1e0dbaf0e9e92a21347c5
Kulametov Enver <zizu.meridian@gmail.com>
2022-12-27 11:45:55 +0300

    change loot type

--- sprint3/problems/find_return/solution/src/model/loot.h
+++ sprint3/problems/find_return/solution/src/model/loot.h
@@ -1,10 +1,9 @@
 #pragma once
-#ifndef __MODEL__LOOT_H__
-#define __MODEL__LOOT_H__
+#ifndef __MODEL__LOOT_TYPE_H__
+#define __MODEL__LOOT_TYPE_H__
 
 #include <string>
-
-#include "geometry.h"
+#include <optional>
 
 namespace model {
 
@@ -15,25 +14,9 @@ struct LootType {
   std::optional<int> rotation;       // example -> = 90;
   std::optional<std::string> color;  // example -> = "#338844";
   std::optional<double> scale;       // example -> = 0.03;
-};
-
-class Loot {
- public:
-  Loot(const LootType& type,const Point2d& pos, const unsigned& loot_index) : m_loot_type(type), m_position(pos), m_loot_index(loot_index) {}
-
-  // void SetPosition(const Point2d& pos);
-
-  const LootType& GetLootType() const noexcept { return m_loot_type;}
-  const Point2d& GetPosition() const noexcept {return m_position;}
-  const unsigned& GetLootIndex() const noexcept {return m_loot_index;}
-
-
- private:
-  const LootType m_loot_type;
-  const Point2d m_position;
-  const unsigned m_loot_index;
+  std::optional<int> value;
 };
 
 }  // namespace model
 
-#endif  // __MODEL__LOOT_H__
+#endif  // __MODEL__LOOT_TYPE_H__
