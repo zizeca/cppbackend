2b5051613654e9a28e0ed67c673329d74bf9dbe1
Kulametov Enver <zizu.meridian@gmail.com>
2023-01-29 20:50:37 +0300

    nassessary export

--- sprint4/problems/leave_game/solution/run.sh
+++ sprint4/problems/leave_game/solution/run.sh
@@ -1,1 +1,3 @@
+
+export GAME_DB_URL=postgres://postgres:Mys3Cr3t@192.168.1.200:30432/test_db
 ./build/bin/game_server --config-file ./data/config.json --www-root static/ --tick-period 50 --randomize-spawn-points --state-file ./save/out.txt --save-state-period 5000
